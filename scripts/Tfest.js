8540caf0329f9aa1433f1ee0af3d44b55e41796a
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2021-12-26 03:40:28 +0300

    add all artists page

---
+++ scripts/Tfest.js
@@ -0,0 +1,38 @@
+let artistJS = document.getElementById("artist_slide");
+let artists = [];
+
+class slide {
+    constructor(sourselink){
+        this.slide_div = document.createElement('div');
+        this.slide_div.className = "slide";
+        this.sourselink = sourselink;
+        this.slide_div.innerHTML =
+        `<img src="${sourselink}" alt="image">`;
+    }
+}
+
+artists[0] = new slide('../../assets/image/tfest1000x500(3).png'); artistJS.append(artists[0].slide_div);
+artists[1] = new slide('../../assets/image/tfest1000x500(2).png'); artistJS.append(artists[1].slide_div);
+artists[2] = new slide('../../assets/image/tfest1000x500(1).png'); artistJS.append(artists[2].slide_div);
+artists[0].slide_div.style.display = "block";
+let number_slide = 0;
+
+function change(n){
+   
+    showSlides(number_slide+=n)
+}
+
+function showSlides(n){
+    let i;
+    if(n>artists.length){
+        number_slide = 1;
+    }
+    if(n<1){
+        number_slide = artists.length;
+    }
+    
+    for(i=0; i<artists.length; i++){
+        artists[i].slide_div.style.display = "none";
+    }
+    artists[number_slide-1].slide_div.style.display = "block";
+}
---
+++ scripts/TfestPlayer.js
@@ -0,0 +1,93 @@
+const player = document.querySelector('.player'),
+    playBtn = document.querySelector('.play'),
+    prevBtn = document.querySelector('.prevsong'),
+    nextBtn = document.querySelector('.nextsong'),
+    pauseBtn = document.querySelector('pause'),
+    audio = document.querySelector('audio'),
+    progressContainer = document.querySelector('.progress__container'),
+    progress = document.querySelector('.progress'),
+    title = document.querySelector('.song'),
+    cover = document.querySelector('.cover__img'),
+    imgSrc = document.querySelector('.img__src')
+
+const PauseBUTTON = document.getElementById("pause");
+const PlayBUTTON = document.getElementById("play");
+
+const songs = ['T-Fest feat. Баста - Скандал','T-Fest & Скриптонит - Ламбада','T-Fest - Не Для Меня']
+
+let songIndex = 0;
+
+function loadSong(song){
+    title.innerHTML = song
+    cover.src = `../../assets/image/cover${songIndex+13}.jpg`
+    audio.src = `../../assets/music/${song}.mp3`
+    
+}
+
+loadSong(songs[songIndex])
+
+function playSong(){
+    player.classList.add('play')
+    cover.classList.add('active')
+    audio.play();
+}
+
+function pauseSong(){
+    
+    player.classList.remove('play')
+    cover.classList.remove('active')
+    
+    audio.pause();
+    
+}
+
+playBtn.addEventListener('click', ()=>{
+    const isPlaying = player.classList.contains('play');
+    if(isPlaying){
+        pauseSong()
+    }else{
+        playSong()
+    }
+})
+    //PlayBUTTON.style.display = "none"
+    //PauseBUTTON.style.display = "block"
+
+    function nextSong(){
+        songIndex++
+
+        if(songIndex > songs.length-1){
+            songIndex =0;
+        }
+
+        loadSong(songs[songIndex])
+        playSong()
+    }
+
+    function prevSong(){
+        songIndex--
+        if(songIndex<0){
+            songIndex = songs.length -1
+        }
+        loadSong(songs[songIndex])
+        playSong()
+    }
+
+    function updateProgress(e){
+        const {duration, currentTime} = e.srcElement
+        const progressPercent = (currentTime/duration)*100;
+        progress.style.width =`${progressPercent}%`
+    }
+    audio.addEventListener('timeupdate',updateProgress)
+
+
+    function setProgress(e){
+        const width = this.clientWidth;
+        const clickX = e.offsetX;
+        const duration = audio.duration
+
+        audio.currentTime = (clickX/width)*duration
+        
+    }
+    progressContainer.addEventListener('click',setProgress)
+
+    audio.addEventListener('ended',nextSong)
